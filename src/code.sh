8c678832372f0343a2922f4cf628314b74d688a5
isabelrem <irt99@protonmail.com>
2025-12-03 10:28:43 +0000

    updated scip docker image

--- src/code.sh
+++ src/code.sh
@@ -12,7 +12,7 @@ dx-download-all-inputs --parallel
 mkdir -p ~/out ./genome ~/out/all_outputs ~/out/html_reports
 
 # download SCIP docker image 
-scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J4gV76803P8X87GGJbBKpgj2
+scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J4j10z803P8VZZgQGvFvX76P
 dx download ${scip_docker_file_id}
 
 ls
