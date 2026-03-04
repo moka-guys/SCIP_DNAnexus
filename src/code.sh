5873811adb4ed8f6e3c91fa96b8fe87bea7d0810
isabelrem <irt99@protonmail.com>
2026-03-04 13:34:57 +0000

    new SCIP docker container with ability to use non-FE results if FE analysis fails, using a more specific error type to identify the failure

--- src/code.sh
+++ src/code.sh
@@ -12,7 +12,7 @@ dx-download-all-inputs --parallel
 mkdir -p ~/out ./genome ~/out/all_outputs ~/out/html_reports
 
 # download SCIP docker image 
-scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J6Z1vQ803P8zzxFJKkb1V5vg
+scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J6Z33Fj03P8fk7J0BY7Z7PpJ
 dx download ${scip_docker_file_id}
 
 ls
