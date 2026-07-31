89c22c257eed4a5d9df558de86e47306b66effb4
isabelrem <irt99@protonmail.com>
2026-07-31 14:34:48 +0100

    changes to N_M_155first

--- src/code.sh
+++ src/code.sh
@@ -12,7 +12,7 @@ dx-download-all-inputs --parallel
 mkdir -p ~/out ./genome ~/out/all_outputs ~/out/html_reports
 
 # download SCIP docker image 
-scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J6Z33Fj03P8fk7J0BY7Z7PpJ
+scip_docker_file_id=project-Gkvkbjj03P8qxxk16qb1yqqQ:file-J9fB6vQ03P8qqX6FYXKzZF9q
 dx download ${scip_docker_file_id}
 
 ls
